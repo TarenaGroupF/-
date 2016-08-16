5de6c7da33effac451ec107b2b30545244ad95c4
guanxindegithub <tarena160102@weilebuluo.local>
2016-08-16 12:01:01 +0800

    卧槽
    发大水发放

---
+++ git/git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by tarena160102 on 16/8/16.
+//  Copyright © 2016年 tarena160102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git/git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git
+//
+//  Created by tarena160102 on 16/8/16.
+//  Copyright © 2016年 tarena160102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
